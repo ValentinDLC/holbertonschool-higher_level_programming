0aa4a7c5f22051128da6edbc6b44a7c22189a9cc
ValentinDLC <valentin.dardenne9@gmail.com>
2025-09-16 15:55:36 +0200

    Fix code

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -3,22 +3,26 @@
 This module has the Square class with checks for size.
 """
 
+
 class Square:
     """Represents a square and makes sure the size is valid.
-
+    
     Attributes:
-        __size (int): The size of the square 
+        __size (int): The size of the square
     """
-
+    
     def __init__(self, size=0):
         """Create a new Square and check the size is okay.
-        Args: size(int): the size for the square by default to 0
         
-        Raises: TypeError: If size isn't an integer.
-                ValueError: If size is less than 0.
-         """
+        Args:
+            size (int): the size for the square, default is 0
+            
+        Raises:
+            TypeError: If size isn't an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if size < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
