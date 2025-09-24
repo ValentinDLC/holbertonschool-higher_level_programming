4067ee307ef1024b5413dfe5f0a972587f1dd65e
ValentinDLC <valentin.dardenne9@gmail.com>
2025-09-24 19:23:15 +0200

    Add inherits_from for strict subclass check

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a base class for geometry objects.
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometry.
+    Currently empty, meant to be extended later.
+    """
+    pass
