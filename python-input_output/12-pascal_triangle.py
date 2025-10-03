1ecff672cd01282315e656891eedeb82ba53d644
ValentinDLC <valentin.dardenne9@gmail.com>
2025-10-03 22:47:01 +0200

    fix problem

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -2,17 +2,17 @@
 """Module that generates Pascal's Triangle"""
 
 def pascal_triangle(n):
-    """Return a list of list of integer representing Pascal's triangle of n"""
+    """Return a list of lists of integers representing Pascal's triangle of n"""
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
 
     for i in range(1, n):
         row = [1]
-        for j in range (1, i):
-            row.append(triangle[i-1][j-1])
-            row.append(1)
-            triangle.append(row)
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
     return triangle
