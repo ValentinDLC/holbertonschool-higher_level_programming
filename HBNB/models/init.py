0a122c9ca2ae12479b457c845f7761a7e5f5585a
ValentinDLC <valentin.dardenne9@gmail.com>
2025-10-24 23:40:36 +0200

    change code fail

--- HBNB/models/init.py
+++
@@ -1,5 +0,0 @@
-# Importe les classes principales (sans cycles)
-from .base_model import BaseModel
-from .user import User
-from .amenity import Amenity
-# Place et Review importés via Facade pour éviter cycles
--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""List all states from the database hbtn_0e_0_usa"""
+"""Lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    
-    # Execute query to get all states ordered by id
-    cursor.execute("SELECT * FROM states ORDER BY id ASC;")
-    
-    # Fetch and print all results
-    for state in cursor.fetchall():
-        print(state)
-
-    # Close connection
-    cursor.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
     db.close()
