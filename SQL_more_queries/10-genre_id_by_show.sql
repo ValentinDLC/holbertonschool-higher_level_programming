e6dc5fa2bd01fbcd996f169b5ac56ef9bc190b14
ValentinDLC <valentin.dardenne9@gmail.com>
2025-10-24 22:30:31 +0200

    List all shows with at least one genre, sorted by title and genre_id

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,11 @@
+-- This script lists all TV shows that have at least one genre linked.
+-- We select the show title and the genre_id from tv_show_genres.
+-- Only shows with genres are displayed.
+-- Results are sorted by tv_shows.title and genre_id ascending.
+
+USE hbtn_0d_tvshows;
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
