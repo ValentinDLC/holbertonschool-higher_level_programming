6c91f47933d9301ce2fe1394382c7b0a4a6bdf88
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-16 12:32:55 +0100

    fix with pycodestyle and string formatting

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end="")
-        else:
-            print(char, end="")
-    print()
+    print("{}".format("".join(chr(ord(c) - 32) if 'a' <= c <= 'z' else c
+                              for c in str)))
