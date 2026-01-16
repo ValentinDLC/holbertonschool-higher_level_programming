ee183954fa8511201fc1bdc5e31c8c2a60251397
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-16 10:11:27 +0100

    Prints number of command-line arguments and lists them

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Prints the number of command-line arguments and lists them.
+"""
+
+import sys
+
+if __name__ == "__main__":
+    argc = len(sys.argv) - 1
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print("{} argument{}:".format(argc, "" if argc == 1 else "s"))
+        for i, arg in enumerate(sys.argv[1:], 1):
+            print("{}: {}".format(i, arg))
