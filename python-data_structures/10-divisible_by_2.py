a59140999c113533e509a1df544d7eb9035f6372
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-20 11:51:21 +0100

    Function to check multiples of 2 in a list

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result_list = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            result_list.append(True)
+        else:
+            result_list.append(False)
+
+    return result_list
+"""  
+        return [i % 2 == 0 for i in my_list]
+"""
