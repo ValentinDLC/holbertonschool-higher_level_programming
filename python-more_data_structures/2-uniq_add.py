fafd6429603e2bf9991483350a67630899c3e106
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-20 15:26:22 +0100

    Implementfunction to sum unique elements of a list

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_elements = []
+    total_sum = 0
+
+    for element in my_list:
+        if element not in uniq_elements:
+            uniq_elements.append(element)
+            total_sum += element 
+
+    return total_sum
