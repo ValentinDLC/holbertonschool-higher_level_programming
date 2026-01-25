9325a368ad13b43208da43a13f0a56191cec432c
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-25 22:22:25 +0100

    fix pycodestyle

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,12 +2,9 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    
     m_v = my_list[0]
 
     for v in my_list[1:]:
         if v > m_v:
             m_v = v
-        
     return m_v
-
