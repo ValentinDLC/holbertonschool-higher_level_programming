6bf7f135308af1db253db81c0234caab98d04037
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-28 10:26:05 +0100

    fix - lower

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -35,9 +35,9 @@ class Square:
             ValueError: If value is less than 0
         """
         if not isinstance(value, int):
-            raise TypeError("Size must be an integer")
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise ValueError("Size must be >= 0")
+            raise ValueError("size must be >= 0")
 
         self.__size = value
 
