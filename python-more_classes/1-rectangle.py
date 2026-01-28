9bd14cdcfb0bf81bce4dcd13aa242fba00a3375d
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-28 10:41:07 +0100

    Fix - complet isinstance validation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -65,7 +65,7 @@ class Rectangle:
         Args:
             value (int): The new height
         """
-        if not isinstance:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
