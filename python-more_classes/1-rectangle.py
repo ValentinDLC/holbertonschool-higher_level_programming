30bcce52985d2390bb6953a9c471bcff3e3c0fbd
ValentinDLC <valentin.dardenne9@gmail.com>
2026-01-28 10:38:53 +0100

    Fix - Correct width typo and validation

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     """
     Represents a rectangle
     """
-    def __init__(self, widht=0, height=0):
+    def __init__(self, width=0, height=0):
         """
         Initialize a new Rectangle instance
 
@@ -16,21 +16,21 @@ class Rectangle:
             width (int): Width of the rectangle (default is 0)
             height (int): Height of the rectangle (default is 0)
         """
-        self.width = widht
+        self.width = width
         self.height = height
 
     @property
-    def widht(self):
+    def width(self):
         """
         Get the width of the rectangle
 
         Returns:
             int: Width of the rectangle
         """
-        return self.__widht
+        return self.__width
 
-    @widht.setter
-    def widht(self, value):
+    @width.setter
+    def width(self, value):
         """
         Set the width of the rectangle
 
@@ -41,11 +41,11 @@ class Rectangle:
             TypeError: If value is not an integer
             ValueError: If value is less than 0
         """
-        if not isinstance:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__widht = value
+        self.__width = value
 
     @property
     def height(self):
