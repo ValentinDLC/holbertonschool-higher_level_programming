ecd96c50181b1e8b1f5443ffff402fe805ad288a
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-06 23:43:23 +0100

    Print first State object ordered by id via SQLAlchemy

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
-"""Print the first State object from the database or 'Nothing' if empty.
-Studious English comments.
-"""
+"""Prints the first State object from a given database, ordered by id."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Efficiently fetch first object ordered by id without fetching all
-    first_state = session.query(State).order_by(State.id).first()
-    if first_state:
-        print(f"{first_state.id}: {first_state.name}")
-    else:
-        print("Nothing")
+    state = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(state.id, state.name) if state else "Nothing")
 
     session.close()
