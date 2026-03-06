d528b6923d37b803b2120791e8c67efd7586b919
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-06 23:34:24 +0100

    Filter states by name argument using format

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,24 @@
 #!/usr/bin/python3
-"""List states that match the argument"""
-import MySQLdb
+"""Lists all states matching a given name argument, sorted by id."""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    
-    # Build query using format (unsafe version)
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(sys.argv[4])
-    cursor.execute(query)
-    
-    for state in cursor.fetchall():
-        print(state)
-    
-    cursor.close()
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            sys.argv[4]
+        )
+    )
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
     db.close()
