999f24d53686d86d952124e6c6babd0d8e32ef00
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-06 23:41:30 +0100

    List all State objects sorted by id via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     session = Session()
 
     for state in session.query(State).order_by(State.id).all():
-        print(f"{state.id}: {state.name}")
+        print("{}: {}".format(state.id, state.name))
 
     session.close()
