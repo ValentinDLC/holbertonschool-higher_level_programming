794efa8cb8af1e9854c9bcae0b974450239fe555
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-06 23:38:19 +0100

    List all cities of a given state safely using parameterized query

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""Print all cities of a given state (filter by state name)
-Safe from SQL injection and uses only one execute() call.
-"""
-import MySQLdb
+"""Lists all cities of a given state, safe from SQL injection."""
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Connect to MySQL
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-
-    # Parameterized query to avoid SQL injection (single execute)
-    query = ("SELECT cities.name "
-             "FROM cities "
-             "JOIN states ON cities.state_id = states.id "
-             "WHERE states.name = %s "
-             "ORDER BY cities.id ASC;")
-    cursor.execute(query, (sys.argv[4],))
-
-    rows = cursor.fetchall()
-    # If any results, join city names with comma and space
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.name "
+        "FROM cities "
+        "JOIN states ON cities.state_id = states.id "
+        "WHERE states.name = %s "
+        "ORDER BY cities.id ASC",
+        (sys.argv[4],)
+    )
+    rows = cur.fetchall()
     if rows:
-        city_names = ", ".join(row[0] for row in rows)
-        print(city_names)
-
-    cursor.close()
+        print(", ".join(row[0] for row in rows))
+    cur.close()
     db.close()
