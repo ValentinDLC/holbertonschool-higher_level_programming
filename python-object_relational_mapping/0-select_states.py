b5702db13bc13677c691659d60293b3627cefc2d
ValentinDLC <valentin.dardenne9@gmail.com>
2026-03-06 23:27:51 +0100

    Add utf8 charset to MySQLdb connection

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3]
+        db=sys.argv[3],
+        charset="utf8"
     )
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
@@ -17,4 +18,3 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
-    
